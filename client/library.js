b31ded1313fc5972a226981805ce6eecd62575be
Scott Hughes <scott_t_hughes@outlook.com>
2024-02-13 13:01:52 +1100

    Markers added

---
+++ client/library.js
@@ -0,0 +1,23 @@
+// export default function renderMarkers() {
+
+//   fetch('http://localhost:8080/api/stations/all')
+//     .then(res => res.json())
+//     .then(stations => {
+
+//       for(let station of stations) {
+//         let myLatLng = {
+//           lat: Number(station.latitude),
+//           lng: Number(station.longitude)
+//         }
+        
+//         new google.maps.Marker({
+//           position: myLatLng,
+//           map,
+//           title: station.name
+//         })
+        
+//       }
+
+
+//     })
+// }
--- client/map.js
+++ client/map.js
@@ -1,13 +1,44 @@
+
+
 let map;
 
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: -38.400654164953046, lng: 146.15877161420863 },
     zoom: 13,
     minZoom: 9,
   });
+
+
+  renderMarkers()
 }
 
 initMap();
+
+function renderMarkers() {
+
+  fetch('http://localhost:8080/api/stations/all')
+    .then(res => res.json())
+    .then(stations => {
+
+      for(let station of stations) {
+        let myLatLng = {
+          lat: Number(station.latitude),
+          lng: Number(station.longitude)
+        }
+        
+        new google.maps.Marker({
+          position: myLatLng,
+          map,
+          title: station.name
+        })
+        
+      }
+
+
+    })
+}
+
+
