8d340889adb129233fb8cc6f6f4ce1ef1793daf8
leotta <t.ryo0212@gmail.com>
2024-02-15 10:41:29 +1100

    fix bug on weather

--- client/features/weather.js
+++ client/features/weather.js
@@ -1,6 +1,17 @@
-export function getCurrentWeather() {
-  let lat = Number(document.querySelector('.map-center > h5 > span').textContent)
- let lng = Number(document.querySelector('.map-center > h5 + h5 > span').textContent)
+const mapCenterLocationWeather = document.querySelector('.map-center-location-weather > button')
+mapCenterLocationWeather.addEventListener('click', handleClickWeather)
+ 
+function handleClickWeather() {
+  getCenterWeather()
+}
+
+export function getCurrentWeather(map) {
+  // let lat = Number(document.querySelector('.map-center > h5 > span').textContent)
+  // let lng = Number(document.querySelector('.map-center > h5 + h5 > span').textContent)
+
+  let latlng = map.getCenter()
+  let lat = latlng.lat()
+  let lng = latlng.lng()
 
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=347533d0e42725230e0bb151a7cb2eea`)
   .then(res => res.json())
@@ -9,22 +20,35 @@ export function getCurrentWeather() {
     let location = result.name
     let humidity = result.main.humidity
 
-    const currentWather = document.querySelector('.current-weather')
+    let cityElem = document.querySelector('.user-location-weather > p > span')
+    cityElem.textContent = location
 
+    let tempElem = document.querySelector('.user-location-weather > p + p > span')
+    tempElem.textContent = temp
 
-    let locationElem = document.createElement('h5')
-    locationElem.textContent = 'Location: ' + location
-
-    let tempElem = document.createElement('h5')
-    tempElem.textContent = 'Temperature: ' + temp
-
-    let humidityElem = document.createElement('h5')
-    humidityElem.textContent = 'Humidity: ' + humidity
-
-    currentWather.appendChild(locationElem)
-    currentWather.appendChild(tempElem)
-    currentWather.appendChild(humidityElem)
-
+    let humidityElem = document.querySelector('.user-location-weather > p + p + p > span')
+    humidityElem.textContent = humidity
   })
+}
 
+export function getCenterWeather() {
+  let lat = Number(document.querySelector('.map-center > h5 > span').textContent)
+  let lng = Number(document.querySelector('.map-center > h5 + h5 > span').textContent)
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=347533d0e42725230e0bb151a7cb2eea`)
+  .then(res => res.json())
+  .then(result => {
+    let temp = result.main.temp
+    let location = result.name
+    let humidity = result.main.humidity
+
+    let cityElem = document.querySelector('.map-center-location-weather > p > span')
+    cityElem.textContent = location
+
+    let tempElem = document.querySelector('.map-center-location-weather > p + p > span')
+    tempElem.textContent = temp
+
+    let humidityElem = document.querySelector('.map-center-location-weather > p + p + p > span')
+    humidityElem.textContent = humidity
+  })
 }
