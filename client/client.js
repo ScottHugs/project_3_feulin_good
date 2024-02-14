130eec9044aecdc90e1b009963f2be02a5869344
Jason <jaztruong@gmail.com>
2024-02-14 14:35:18 +1100

    stats done

--- client/client.js
+++ client/client.js
@@ -43,7 +43,41 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?lat=-38.40065416&lon=146.
     currentWather.appendChild(tempElem)
     currentWather.appendChild(humidityElem)
 
+  })
 
+fetch('http://localhost:8080/api/stats')
+  .then(res => res.json())
+  .then(stats => {
+    let totalStations = stats.total_stations
+    let totalOwners = stats.total_owners
+
+    let totalStationsElem = document.createElement('h4')
+    totalStationsElem.textContent = 'total station: ' + totalStations
+
+    let totalOwnersElem = document.createElement('h4')
+    totalOwnersElem.textContent = 'total owners: ' + totalOwners
+
+    let statsElem = document.querySelector('.stats')
+
+    statsElem.appendChild(totalStationsElem)
+    statsElem.appendChild(totalOwnersElem)
+
+    let totalStationsByOwner = stats.owners
+    for (let item of totalStationsByOwner) {
+      let owner = item.owner
+      let total = item.count
+      let ownerElem = document.createElement('h5')
+      ownerElem.textContent = owner
+      let totalElem = document.createElement('h5')
+      totalElem.textContent = total
+
+      let ownerDivElem = document.createElement('div')
+      ownerDivElem.classList.add('owner-stations')
+
+      statsElem.appendChild(ownerDivElem)
+      ownerDivElem.appendChild(ownerElem)
+      ownerDivElem.appendChild(totalElem)
+    }
   })
 
 
--- db/playground.js
+++ db/playground.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const Station = require('../models/station')
 const Owner = require('../models/owner')
+const Stats = require('../models/stats')
 
 // Station.findAll()
 //   .then(stations => console.log(stations))
@@ -9,5 +10,8 @@ const Owner = require('../models/owner')
 // Owner.findOwners()
 // .then(owners => console.log(owners))
 
-Station.findRandom()
-  .then(station => console.log(station))
+// Station.findRandom()
+//   .then(station => console.log(station))
+
+Stats.totalStationsByOwner()
+  .then(stats => console.log(stats))
